55b71c15fca839c4055e2ec359949a26d7278bbe
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2023-11-27 23:42:55 +0600

    Worked on custom permissions

---
+++ pj22/api/views.py
@@ -0,0 +1,13 @@
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from api.custompermissions import MyPermission
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [MyPermission]
+    
